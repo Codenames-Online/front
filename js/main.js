31e89af758600e2d26b3404f4a228054bc14a864
Viktor Koves <viktorkoves@gmail.com>
2017-11-04 16:24:32 -0500

    Added voting system UI and demo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 var admin = false;
+var myTeam = "blue";
+
+$(document).ready(function() {
+	$('.card').click(selectCard);
+	$('.card').contextmenu(selectCardTeam);
+});
+
+function selectCard() {
+	$('.card.selected.self .self-select').remove(); // delete self select icon
+	$('.card.selected.self').removeClass('selected self'); //de self select
+	$(this).addClass('selected self');
+
+	// append self icon to make it last
+	if(myTeam == "blue")
+		$(this).find('.icon-cont').append('<div class="self-select blue"></div>');
+	else
+		$(this).find('.icon-cont').append('<div class="self-select red"></div>');
+}
+
+function selectCardTeam(event) {
+	$(this).addClass('selected team');
+
+	// prepend team icons to make them first
+	if(myTeam == "blue")
+		$(this).find('.icon-cont').prepend('<div class="team-select blue"></div>');
+	else
+		$(this).find('.icon-cont').prepend('<div class="team-select red"></div>');
+}
+
 
 function toggleAdmin() {
 	if(admin) // remove admin by clearing agent values
