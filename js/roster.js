b7ccbeec5d5bf275f10b3240f669beacb6c08d3f
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-11-05 02:06:39 -0600

    Don't need to get names now

--- js/roster.js
+++ js/roster.js
@@ -1,17 +1,17 @@
 // @param teams - {blue: [String], red: [String]} contains two arrays of String names
 function updateRoster(teams) {
-	var blueRoster = teams.blue; //.sort();
-	var redRoster = teams.red; //.sort();
+	var blueRoster = teams.blue.sort();
+	var redRoster = teams.red.sort();
 
 	// Clear old rosters by clearing all player objects in rosters
 	$('.players .player').remove()
 
 	for(var i = 0; i < blueRoster.length; i++) {
-		appendToRoster('blue', blueRoster[i].name);
+		appendToRoster('blue', blueRoster[i]);
 	}
 	
 	for(var i = 0; i < redRoster.length; i++) {
-		appendToRoster('red', redRoster[i].name);
+		appendToRoster('red', redRoster[i]);
 	}
 
 	function appendToRoster(team, name) {
