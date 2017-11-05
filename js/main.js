c5b35299a0c026bc8f41bc6ee563ee81ce19be96
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-11-04 20:51:36 -0500

    Add Websocket connection

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
+var me, socket;
 
+$(document).ready(function() {
+	setupSocket();
+	$('.registration-page-cont .btn').click(goToRoster);
 });
+
+function setupSocket() {
+	// Create WebSocket connection - TODO: unhard code this
+	socket = new WebSocket(`ws://${window.location.hostname}:3000`);
+
+	// Connection opened
+	socket.addEventListener('open', function (event) {
+			// socket.send({ greeting: 'Hello Server!'});
+	});
+
+	// Listen for messages
+	socket.addEventListener('message', function (event) {
+			console.log('Message from server ', event.data);
+	});
+}
+
+function goToRoster() {
+	
+}
