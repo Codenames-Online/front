e0e796b0dccdcea4570cff99224a38dd270e2b59
Viktor Koves <viktorkoves@gmail.com>
2017-12-24 17:44:30 -0600

    Moved out lobby-registartion JS to its own file Also fixed lobby-registration showing up below the normal registration UI

--- js/pages/registration.js
+++ js/pages/registration.js
@@ -19,41 +19,6 @@ function registerName() {
 	}
 }
 
-function goToLobbyRegistration() {
-	$('.registration-page-cont').hide();
-	$('.lobby-registration-page-cont').show();
-}
-
-function createLobby() {
-	try {
-		sendSocket({ action: "createLobby" });
-	}
-	catch(error) {
-		console.error(error);
-		showOverlay('websocket-error');
-	}
-}
-
-function joinLobby() {
-	if(!$('#joinLobby').hasClass('disabled')) { // only continue if button is enabled
-		var lobby = $('.lobby-registration-page-cont input').val();
-		lobby = escapeHtml(lobby); // escape name for safety
-
-		try {
-			sendSocket({ action: "joinLobby", gid: lobby });
-		}
-		catch(error) {
-			console.error(error);
-			showOverlay('websocket-error');
-		}
-	}
-}
-
-function goToRoster() {
-	$('.lobby-registration-page-cont').hide();
-	$('.roster-page-cont').show();
-}
-
 function checkEnteredName() {
 	var name = $('.registration-page-cont input').val();
 
